b2888aef1a6b0fc89d00da0e4a5ded824cb0f024
akio0911 <akio0911@gmail.com>
2021-06-20 13:59:18 +0900

    feedback

--- Salon_Task1/ViewController.swift
+++ Salon_Task1/ViewController.swift
@@ -9,44 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var firstText: UITextField!
-    @IBOutlet weak var secondText: UITextField!
-    @IBOutlet weak var thirdText: UITextField!
-    @IBOutlet weak var fourthText: UITextField!
-    @IBOutlet weak var fifthText: UITextField!
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+    @IBOutlet weak var thirdTextField: UITextField!
+    @IBOutlet weak var fourthTextField: UITextField!
+    @IBOutlet weak var fifthTextField: UITextField!
     @IBOutlet weak var answerLabel: UILabel!
-    
+
+    private var textFields: [UITextField] {
+        [firstTextField, secondTextField, thirdTextField, fourthTextField, fifthTextField]
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        answerLabel.text = "0"
-        
-        self.firstText.keyboardType = UIKeyboardType.numberPad
-        self.secondText.keyboardType = UIKeyboardType.numberPad
-        self.thirdText.keyboardType = UIKeyboardType.numberPad
-        self.fourthText.keyboardType = UIKeyboardType.numberPad
-        self.fifthText.keyboardType = UIKeyboardType.numberPad
 
+        answerLabel.text = "0"
+
+        textFields.forEach {
+            $0.keyboardType = .numberPad
+        }
     }
 
     @IBAction func buttonAction(_ sender: Any) {
-        
-        let firstNum = NumberFormatter().number(from: firstText.text ?? "0") as? Int ?? 0
-        let secondNum = NumberFormatter().number(from: secondText.text ?? "0") as? Int ?? 0
-        let thirdNum = NumberFormatter().number(from: thirdText.text ?? "0") as? Int ?? 0
-        let fourthNum = NumberFormatter().number(from: fourthText.text ?? "0") as? Int ?? 0
-        let fifthNum = NumberFormatter().number(from: fifthText.text ?? "0") as? Int ?? 0
-        
-        let numbers = [firstNum, secondNum, thirdNum, fourthNum, fifthNum]
-        
+        let numberFormatter = NumberFormatter()
+
+        let numbers = textFields
+            .map { numberFormatter.number(from: $0.text ?? "")?.intValue ?? 0 }
+
         answerLabel.text = "\(addition(numbers: numbers))"
     }
-    
-    func addition(numbers: [Int]) -> Int {
-        
-        let result: Int = numbers.reduce(0) { $0 + $1 }
 
-        return result
+    func addition(numbers: [Int]) -> Int {
+        numbers.reduce(0, +)
     }
 }
-
